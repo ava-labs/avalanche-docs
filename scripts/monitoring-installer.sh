747c721c365d3b99175d16d5e5795c2b65d3bcab
Dorzhi <39804288+tdorzhi@users.noreply.github.com>
2021-11-21 09:34:14 +0300

    Update monitoring-installer.sh

--- scripts/monitoring-installer.sh
+++ scripts/monitoring-installer.sh
@@ -84,11 +84,11 @@ install_prometheus() {
   fi
   echo "Attempting to download: $promFileName"
   wget -nv --show-progress -O prometheus.tar.gz $promFileName
-  mkdir prometheus
+  mkdir -p prometheus
   tar xvf prometheus.tar.gz -C prometheus --strip-components=1
   echo "Installing..."
-  sudo useradd -M -r -s /bin/false prometheus
-  sudo mkdir /etc/prometheus /var/lib/prometheus
+  id -u prometheus &>/dev/null || sudo useradd -M -r -s /bin/false prometheus
+  sudo mkdir -p /etc/prometheus /var/lib/prometheus
   sudo apt-get install -y apt-transport-https
   cd prometheus
   sudo cp {prometheus,promtool} /usr/local/bin/
