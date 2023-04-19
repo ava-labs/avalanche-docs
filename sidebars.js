ca4d52c44f3db95bed9b04ea190094c44cb67516
Gabriel Cardona <gabriel@avalabs.org>
2023-04-19 12:37:15 -0700

    Recreating https://github.com/ava-labs/avalanche-docs/pull/1267 (#1310)

--- sidebars.js
+++ sidebars.js
@@ -158,6 +158,7 @@ const sidebars = {
         'subnets/create-a-vm-timestampvm',
         'subnets/create-a-vm-blobvm',
         'subnets/create-a-simple-rust-vm',
+        'subnets/create-a-simple-vm-from-scratch',
       ],
     },
     {
