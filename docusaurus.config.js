93aba4d929e57ee41fd8371349f6dfec46ed1403
Meaghan <meag.fitz@avalabs.org>
2023-05-30 17:17:25 -0400

    fix: navbar item size adjustment, remove language options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,10 +136,6 @@ const config = {
             label: "Community",
           },
           {
-            type: "localeDropdown",
-            position: "right",
-          },
-          {
             href: "https://chat.avax.network/",
             className: "header-discord-link",
             position: "right",
