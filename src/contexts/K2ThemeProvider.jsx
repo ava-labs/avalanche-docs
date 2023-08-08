6e868477e877a8c573ac34d2fa1e818be575f100
Connor Chevli <connorchev@gmail.com>
2023-08-08 19:05:56 -0400

    fix: cleanup:

--- src/contexts/K2ThemeProvider.jsx
+++ src/contexts/K2ThemeProvider.jsx
@@ -1,5 +1,4 @@
 import { ThemeProvider, darkTheme, lightTheme } from "@avalabs/k2-components";
-import { useColorMode } from "@docusaurus/theme-common";
 import { useK2Theme } from "../hooks/useK2Theme";
 
 const customTheme = {};
