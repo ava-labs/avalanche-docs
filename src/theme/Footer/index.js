592bd9f0ec47bfcd758b42a0e4836ac590a8d85b
meaghan <meag.fitz@avalabs.org>
2023-08-09 17:56:49 -0400

    added links and subsections

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -36,8 +36,34 @@ function Footer() {
       <div className="container container--fluid"></div>
       <div class="row">
         <div class="col col--4">
-          <div class="col-demo" style={{ backgroundColor: "lightpink" }}>
-            col--3
+          <div className="footer-row">
+            <Link
+              className="footer-main-list"
+              href="https://subnets.avax.network/"
+            >
+              <h3>Explorer</h3>
+            </Link>
+          </div>
+          <div className="footer-row">
+            <Link
+              className="footer-main-list"
+              href="https://academy.avax.com/?utm_source=avalanche-dev-docs&utm_medium=website&utm_content=footer"
+            >
+              <h3>Academy</h3>
+            </Link>
+          </div>
+          <div className="footer-row">
+            <Link
+              className="footer-main-list"
+              href="https://support.avax.network/en/"
+            >
+              <h3>Support</h3>
+            </Link>
+          </div>
+          <div className="footer-row">
+            <Link className="footer-main-list" href="https://core.app/en/">
+              <h3>Wallet</h3>
+            </Link>
           </div>
         </div>
         <div class="col col--7 col--offset-1">
@@ -67,15 +93,42 @@ function Footer() {
               <BsFacebook className="footer-social"></BsFacebook>
             </a>
           </div>
-          <div className="row" style={{ backgroundColor: "lightpink" }}>
-            <div class="col">
-              <div class="col-demo">col</div>
+          <div className="row" style={{ marginTop: "10px" }}>
+            <div class="col" style={{}}>
+              <h4 className="footer-heading">Developers</h4>
+              <a href="https://forum.avax.network/">
+                <div className="footer-list">Forum</div>
+              </a>
+              <a href="https://hackenproof.com/avalanche/avalanche-protocol">
+                <div className="footer-list">Bug Bounty</div>
+              </a>
+              <a href="https://www.avalabs.org/whitepapers">
+                <div className="footer-list">Whitepapers</div>
+              </a>
             </div>
             <div class="col">
-              <div class="col-demo">col</div>
+              <h4 className="footer-heading">Community</h4>
+              <a href="https://medium.com/avalancheavax">
+                <div className="footer-list">Medium</div>
+              </a>
+              <a href="https://docs.avax.network/community/tutorials-contest">
+                <div className="footer-list">Tutorials Contest</div>
+              </a>
+              <a href="https://shop.avax.network/">
+                <div className="footer-list">Merch</div>
+              </a>
             </div>
             <div class="col">
-              <div class="col-demo">col</div>
+              <h4 className="footer-heading">More</h4>
+              <a href="https://avacloud.io/">
+                <div className="footer-list">Enterprise Solutions</div>
+              </a>
+              <a href="https://github.com/ava-labs/audits">
+                <div className="footer-list">Audits</div>
+              </a>
+              <a href="https://www.avax.network/terms-of-use">
+                <div className="footer-list">Terms of Use</div>
+              </a>
             </div>
           </div>
         </div>
@@ -84,8 +137,8 @@ function Footer() {
         <div className="footer__copyright">
           Copyright © {new Date().getFullYear()}{" "}
           <a href="https://avax.network" className="hover:text-yellow">
-            Ava Labs, Inc.{" "}
-          </a>
+            Ava Labs, Inc.
+          </a>{" "}
           <span className="mx-10">|</span> All rights reserved
         </div>
         <div className="footer-logo-container">
