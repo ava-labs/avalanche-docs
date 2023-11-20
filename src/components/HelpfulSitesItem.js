e1655ddf397636bba30c14465f9630d8013c025c
DmytroKoblents <dmytro.koblents@pragmaticdlt.com>
2023-11-20 17:41:37 +0200

    Minor styling correction

--- src/components/HelpfulSitesItem.js
+++ src/components/HelpfulSitesItem.js
@@ -6,8 +6,8 @@ import {translate} from "@docusaurus/core/lib/client/exports/Translate";
 export const HelpfulSiteItem = ({ Svg, link, message }) => {
    return(
     <Link className={styles.usefulLinkContainer} href={link}>
-        <div style={{display:'flex',alignItems:'center',height:'100%',gap:8}}>
-            <Svg style={{width:'24px',height:'24px'}} /> <span className={styles.usefulLinkText}>{translate({message})}</span>
+        <div className={styles.usefulLinkWrapper}>
+            <Svg className={styles.usefulIcon} /> <span className={styles.usefulLinkText}>{translate({message})}</span>
         </div>
     </Link>
    )
