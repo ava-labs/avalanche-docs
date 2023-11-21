dde576631c6cd7c250df7ec71ace7b7d22a83568
meaghan <meag.fitz@avalabs.org>
2023-11-20 19:22:24 -0500

    add function to replace relative links

--- configs/remoteContent.js
+++ configs/remoteContent.js
@@ -1,5 +1,42 @@
 // @ts-check
 
+// Function to replace relative links with absolute links
+function replaceRelativeLinks(content, sourceBaseUrl) {
+  const linkRegex = /\[([^\]]+)\]\(([^)]+)\)|<img[^>]*src=['"]([^'"]*)['"]/g;
+
+  // Replace both markdown-style links and img src attributes with absolute links
+  const updatedContent = content.replace(
+    linkRegex,
+    (match, text, markdownLink, imgSrc) => {
+      if (markdownLink) {
+        if (
+          markdownLink.startsWith("http") ||
+          markdownLink.startsWith("#") ||
+          markdownLink.startsWith("mailto:")
+        ) {
+          // Skip absolute links and anchors
+          return match;
+        }
+
+        // Convert markdown-style relative link to absolute link
+        return `[${text}](${new URL(markdownLink, sourceBaseUrl).href})`;
+      } else if (imgSrc) {
+        if (imgSrc.startsWith("http") || imgSrc.startsWith("data:")) {
+          // Skip absolute links and data URIs
+          return match;
+        }
+
+        // Convert img src attribute relative link to absolute link
+        return `<img src="${new URL(imgSrc, sourceBaseUrl).href}"`;
+      }
+
+      return match;
+    }
+  );
+
+  return updatedContent;
+}
+
 const remoteContent = [
   [
     "docusaurus-plugin-remote-content",
@@ -7,12 +44,16 @@ const remoteContent = [
       // docs/build/cross-chain/teleporter/overview.md
       name: "teleporter-overview",
       sourceBaseUrl:
-        "https://raw.githubusercontent.com/meaghanfitzgerald/translate-ai/master/policies/",
+        "https://raw.githubusercontent.com/ava-labs/teleporter/main/contracts/src/Teleporter/",
       documents: ["README.md"],
       outDir: "docs/build/cross-chain/teleporter/",
-      // change file name and add metadata
+      // change file name and add metadata correct links
       modifyContent(filename, content) {
         if (filename.includes("README")) {
+          const updatedContent = replaceRelativeLinks(
+            content,
+            "https://github.com/ava-labs/teleporter/blob/main/contracts/src/Teleporter/"
+          );
           return {
             filename: "overview.md",
             content: `---
@@ -21,8 +62,8 @@ description: Teleporter is a messaging protocol built on top of Avalanche Warp M
 keywords: [ docs, documentation, avalanche, teleporter, awm, cross-subnet communication, cross-chain, cross-chain communication ]
 sidebar_label: Overview
 ---
-              
-${content}`,
+
+${updatedContent}`,
           };
         }
         return undefined;
