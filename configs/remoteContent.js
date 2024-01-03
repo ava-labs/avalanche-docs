cb1113a0518696bbd312f627be39606ed6f665c7
meaghan <meag.fitz@avalabs.org>
2024-01-03 16:55:09 -0500

    teleporter deep dive

--- configs/remoteContent.js
+++ configs/remoteContent.js
@@ -41,6 +41,38 @@ const remoteContent = [
   [
     "docusaurus-plugin-remote-content",
     {
+      //docs/build/cross-chain/awm/deep-dive.md
+      name: "awm-overview",
+      sourceBaseUrl:
+        "https://raw.githubusercontent.com/ava-labs/avalanchego/master/vms/platformvm/warp/",
+      documents: ["README.md"],
+      outDir: "docs/build/cross-chain/awm/",
+      // change file name and add metadata
+      modifyContent(filename, content) {
+        if (filename.includes("README")) {
+          const updatedContent = replaceRelativeLinks(
+            content,
+            "https://github.com/ava-labs/avalanchego/tree/master/vms/platformvm/warp/"
+          );
+          return {
+            filename: "deep-dive.md",
+            content: `---
+tags: [Avalanche Warp Messaging, AWM, Cross-Subnet Communication, Cross-Chain Communication]
+description: Avalanche Warp Messaging (AWM) provides a primitive for cross-subnet communication on the Avalanche Network.
+keywords: [ docs, documentation, avalanche, awm, cross-subnet communication, cross-chain, cross-chain communication ]
+sidebar_label: Deep Dive
+---
+              
+${updatedContent}`,
+          };
+        }
+        return undefined;
+      },
+    },
+  ],
+  [
+    "docusaurus-plugin-remote-content",
+    {
       // /docs/build/cross-chain/teleporter/overview.md
       name: "teleporter-overview",
       sourceBaseUrl:
@@ -61,6 +93,7 @@ tags: [Teleporter, Cross-Subnet Communication, Cross-Chain Communication]
 description: Teleporter is a messaging protocol built on top of Avalanche Warp Messaging that provides a developer-friendly interface for sending and receiving cross-chain messages from within the EVM.
 keywords: [ docs, documentation, avalanche, teleporter, awm, cross-subnet communication, cross-chain, cross-chain communication ]
 sidebar_label: Overview
+sidebar_position: 1
 ---
 
 ${updatedContent}`,
@@ -73,25 +106,31 @@ ${updatedContent}`,
   [
     "docusaurus-plugin-remote-content",
     {
-      //docs/build/cross-chain/awm/deep-dive.md
-      name: "awm-overview",
+      // /docs/build/cross-chain/teleporter/deep-dive.md
+      name: "teleporter-deep-dive",
       sourceBaseUrl:
-        "https://raw.githubusercontent.com/meaghanfitzgerald/translate-ai/master/scripts/",
+        "https://raw.githubusercontent.com/ava-labs/teleporter/main/",
       documents: ["README.md"],
-      outDir: "docs/build/cross-chain/awm/",
-      // change file name and add metadata
+      outDir: "docs/build/cross-chain/teleporter/",
+      // change file name and add metadata correct links
       modifyContent(filename, content) {
         if (filename.includes("README")) {
+          const updatedContent = replaceRelativeLinks(
+            content,
+            "https://github.com/ava-labs/teleporter/blob/main/"
+          );
           return {
             filename: "deep-dive.md",
             content: `---
-tags: [Avalanche Warp Messaging, AWM, Cross-Subnet Communication, Cross-Chain Communication]
-description: Avalanche Warp Messaging (AWM) provides a primitive for cross-subnet communication on the Avalanche Network.
-keywords: [ docs, documentation, avalanche, awm, cross-subnet communication, cross-chain, cross-chain communication ]
+tags: [Teleporter, Cross-Subnet Communication, Cross-Chain Communication]
+description: Teleporter is an EVM compatible cross-subnet communication protocol built on top of Avalanche Warp Messaging (AWM), and implemented as a Solidity smart contract. It provides a mechanism to asynchronously invoke smart contract functions on other EVM blockchains within Avalanche. Teleporter provides a handful of useful features on top of AWM, such as specifying relayer incentives for message delivery, replay protection, message delivery and execution retries, and a standard interface for sending and receiving messages within a dApp deployed across multiple subnets.
+keywords: [ docs, documentation, avalanche, teleporter, awm, cross-subnet communication, cross-chain, cross-chain communication ]
 sidebar_label: Deep Dive
+sidebar_position: 2
+title: Teleporter Deep Dive
 ---
-              
-${content}`,
+
+${updatedContent}`,
           };
         }
         return undefined;
