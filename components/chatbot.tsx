c2b7bb59ee1a86d3987eeed77a30f8fb3dc1358f
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2024-08-13 19:02:15 +0530

    add kapa chatbot (#1820)

---
+++ components/chatbot.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useEffect } from 'react';
+
+interface KapaWidgetProps {
+  websiteId: string;
+  projectName: string;
+  projectColor: string;
+  projectLogo: string;
+}
+
+const Chatbot: React.FC<KapaWidgetProps> = ({ websiteId, projectName, projectColor, projectLogo }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+    script.setAttribute("data-website-id", websiteId);
+    script.setAttribute("data-project-name", projectName);
+    script.setAttribute("data-project-color", projectColor);
+    script.setAttribute("data-project-logo", projectLogo);
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      if (document.head.contains(script)) {
+        document.head.removeChild(script);
+      }
+    };
+  }, [websiteId, projectName, projectColor, projectLogo]);
+
+  return null;
+};
+
+export default Chatbot;
+
