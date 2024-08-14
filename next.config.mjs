951961e1eef722b67ac2f499e2b491b85e72cf6d
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-08-14 21:00:36 +0530

    commit changes for reference folder

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,21 @@ const config = {
         destination: '/:path*',
         permanent: true,
       },
+      {
+        source: '/reference/:path*',
+        destination: '/api-reference/:path*',
+        permanent: true,
+      },
+      {
+        source: '/reference/avalanchego/:path*',
+        destination: '/api-reference/:path*',
+        permanent: true,
+      },
+      {
+        source: '/reference/subnet-evm/api',
+        destination: '/api-reference/subnet-evm-api',
+        permanent: true,
+      },
     ]
   },
 };
