7bec057a506d8b168ec8b1712d11650dbd163df0
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-08-16 17:48:30 +0530

    redirects for subnets folder

--- next.config.mjs
+++ next.config.mjs
@@ -16,11 +16,6 @@ const config = {
   async redirects() {
     return [
       {
-        source: '/build/:path*',
-        destination: '/:path*',
-        permanent: true,
-      },
-      {
         source: '/reference/avalanchego/p-chain/txn-format',
         destination: '/api-reference/p-chain/txn-format',
         permanent: true,
@@ -83,6 +78,131 @@ const config = {
       {
         source: '/reference/standards/avalanche-network-protocol',
         destination: '/api-reference/standards/avalanche-network-protocol',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/upgrade/upgrade-subnet-vm',
+        destination: '/subnets/upgrade/subnet-virtual-machine',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/upgrade/customize-a-subnet',
+        destination: '/subnets/upgrade/customize-subnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/upgrade/upgrade-durango',
+        destination: '/subnets/upgrade/durango-upgrade',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/upgrade/upgrade-precompile',
+        destination: '/subnets/upgrade/subnet-precompile-config',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/upgrade/considerations-subnet-upgrade',
+        destination: '/subnets/upgrade/considerations',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/elastic/transform-to-elastic-subnet',
+        destination: '/subnets/elastic-subnets/make-subnet-permissionless',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/elastic/elastic-parameters',
+        destination: '/subnets/elastic-subnets/parameters',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/mainnet-subnet',
+        destination: '/subnets/deploy-a-subnet/avalanche-mainnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/local-subnet',
+        destination: '/subnets/deploy-a-subnet/local-network',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/fuji-testnet-subnet',
+        destination: '/subnets/deploy-a-subnet/fuji-testnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/multisig-auth',
+        destination: '/subnets/deploy-a-subnet/multisig-auth',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/custom-vm-subnet',
+        destination: '/subnets/deploy-a-subnet/custom-virtual-machine',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/deploy/on-prod-infra',
+        destination: '/subnets/deploy-a-subnet/production-infrastructure',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/getting-started',
+        destination: '/subnets/index',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/info/troubleshoot-subnet',
+        destination: '/subnets/troubleshooting',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/info/wagmi',
+        destination: '/subnets/wagmi-subnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/maintain/view-subnets',
+        destination: '/subnets/maintain/view-subnets',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/maintain/how-to-transfer-funds',
+        destination: '/subnets/maintain/transfer-pchain-funds',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/maintain/pause-resume-subnet',
+        destination: '/subnets/maintain/pause-resume',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/maintain/delete-subnet',
+        destination: '/subnets/maintain/delete-subnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/c-chain-vs-subnet',
+        destination: '/subnets/c-chain-or-subnet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/utility/cross-chain-evm-bridge',
+        destination: '/subnets/add-utility/cross-chain-bridge',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/utility/avalanche-subnet-faucet',
+        destination: '/subnets/add-utility/testnet-faucet',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/utility/deploy-smart-contract-to-subnet',
+        destination: '/subnets/add-utility/deploy-smart-contract',
+        permanent: true,
+      },
+      {
+        source: '/build/subnet/hello-subnet',
+        destination: '/subnets/build-first-subnet',
         permanent: true,
       },
     ]
