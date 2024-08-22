7c7e28940422fe93cf387e052f6efd6ac07ccd32
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2024-08-22 21:34:26 +0530

    Push changes for 404 page (#1839)
    * push changes for 404 page
    
    * Update app/not-found.tsx
    
    Signed-off-by: Meaghan FitzGerald <meag.fitz@avalabs.org>
    
    ---------
    
    Signed-off-by: Meaghan FitzGerald <meag.fitz@avalabs.org>
    Co-authored-by: Meaghan FitzGerald <meag.fitz@avalabs.org>

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { Layout } from 'fumadocs-ui/layout';
+import { baseOptions } from '@/app/layout.config';
+
+export default function HomePage(): React.ReactElement {
+  return <Layout {...baseOptions}>(
+    <>
+        <main className="flex h-screen flex-col justify-center text-center mt-[150px]">
+        <h1 className="mb-4 text-2xl font-bold">😢{' '}Page Not Found</h1>
+        <p className="text-fd-muted-foreground">
+            Please open an{' '}<Link href="/" className="text-fd-foreground font-semibold underline">issue on GitHub</Link>.<br/>
+            Alternatively, open the{' '}
+            <Link href="/" className="text-fd-foreground font-semibold underline">Homepage</Link>{' '}
+            to find relevant docs.
+        </p>
+        </main>
+    </>
+  );
+  </Layout>;
+}
