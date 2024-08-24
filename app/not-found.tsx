a368d238c6c70f70f7dfdc9d89d31ba67bbb5c16
meaghanfitzgerald <meag.fitz@avalabs.org>
2024-08-23 23:19:58 -0400

    add tsx component library shadcn

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import { baseOptions } from '@/app/layout.config';
 
 export default function HomePage(): React.ReactElement {
   return <Layout {...baseOptions}>
-    <>
+    <div>
         <main className="flex h-screen flex-col text-center mt-[180px]">
         <h1 className="mb-4 text-2xl font-bold">😢{' '}Page Not Found</h1>
         <p className="text-fd-muted-foreground">
@@ -13,7 +13,8 @@ export default function HomePage(): React.ReactElement {
             <Link href="/" className="text-fd-foreground font-semibold underline">Homepage</Link>{' '}
             to find relevant docs.
         </p>
+        <button className="mt-4 text-fd-foreground font-semibold underline"> or go back</button>
         </main>
-    </>
+    </div>
   </Layout>;
 }
