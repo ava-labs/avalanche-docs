28c5b88f3fb3df6d18e740faf819e02a69199673
meaghanfitzgerald <meag.fitz@avalabs.org>
2024-08-24 01:20:56 -0400

    make 404 page funny

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,97 @@
-import Link from 'next/link';
-import { Layout } from 'fumadocs-ui/layout';
-import { baseOptions } from '@/app/layout.config';
+import { Layout } from "fumadocs-ui/layout";
+import { baseOptions } from "@/app/layout.config";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage(): React.ReactElement {
-  return <Layout {...baseOptions}>
-    <div>
-        <main className="flex h-screen flex-col text-center mt-[180px]">
-        <h1 className="mb-4 text-2xl font-bold">😢{' '}Page Not Found</h1>
-        <p className="text-fd-muted-foreground">
-            Please open an{' '}<Link href="/" className="text-fd-foreground font-semibold underline">issue on GitHub</Link>.<br/>
-            Alternatively, open the{' '}
-            <Link href="/" className="text-fd-foreground font-semibold underline">Homepage</Link>{' '}
-            to find relevant docs.
-        </p>
-        <button className="mt-4 text-fd-foreground font-semibold underline"> or go back</button>
-        </main>
-    </div>
-  </Layout>;
+  return (
+    <Layout {...baseOptions}>
+      <div className="relative z-10 container p-10 mx-auto">
+        <div className="flex flex-wrap items-center -mx-4">
+          <div className="column w-full md:w-1/2 px-4 mb-16 md:mb-0">
+            <img
+              className="object-scale-down mx-auto"
+              src="/images/intern-404.png"
+              alt="404"
+            />
+          </div>
+          <div className="column w-full md:w-1/2 px-4 mb-16 md:mb-0">
+            <div className="flex flex-wrap">
+              <div className="md:max-w-xl text-center md:text-left">
+                <span className="inline-block py-px px-2 mb-4 text- s leading font-medium rounded-full shadow-sm bg-red-600">
+                  Error 404
+                </span>
+                <h2 className="mb-4 text-4xl md:text-5xl leading-tight font-bold tracking-tighter">
+                  Seriously?
+                </h2>
+                <p className="mb-6 text-lg md:text-xl">
+                  We told the intern to find this page for you, but he couldn't.
+                  We've warned him about this countless times, and honestly,
+                  we're not sure how much longer we can keep him around.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-auto py-1 lg:py-0 lg:mr-6">
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="outline">Fire Him</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Gotcha. We let Emin know.
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        He should understand the circustances that led to this
+                        decision.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogAction>Docs Home</AlertDialogAction>
+                      <AlertDialogCancel>
+                        Scold Intern on Twitter
+                      </AlertDialogCancel>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+              <div className="w-full lg:w-auto py-1 lg:py-0 lg:mr-6">
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="outline">Let Him Stay</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Hm, interesting choice ...
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        That's awfully nice of you, but we to think about this a
+                        bit more.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogAction>Docs Home</AlertDialogAction>
+                      <AlertDialogCancel>Give Him Advice</AlertDialogCancel>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
