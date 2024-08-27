4c0cb2aea2f24000a42434fa2155294e705d2a5c
Martin Eckardt <martin.eckardt@avalabs.org>
2024-08-27 13:34:11 -0400

    remove bold

--- app/integrations/page.tsx
+++ app/integrations/page.tsx
@@ -52,7 +52,7 @@ export default function Page(): React.ReactElement {
                         {Object.entries(groupedIntegrations).sort().map(([category, integrations]) => (
                             <div key={category} className="mb-12">
                                 <section id={category}>
-                                    <h2 className="text-2xl font-semibold mb-8">{category}</h2>
+                                    <h2 className="text-2xl mb-8">{category}</h2>
                                 </section>
                                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto w-full">
                                     {integrations.map((integration) => (
