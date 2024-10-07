846b7e581d3715168be48c19085e942a7a45e061
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2024-10-07 20:14:16 +0530

    Update providers.js
    Signed-off-by: Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>

--- app/providers.js
+++ app/providers.js
@@ -4,7 +4,8 @@ import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    persistence: 'memory' //enables cookieless tracking
   })
 }
 
