d4f3e16c4bf8e7b35c6872618b47d9b4dbb0f9d6
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-01-13 04:57:13 +0000

    MarshalSubnetToL1ConversionData rewrite

--- components/tools/L1Launcher/DeployContracts/convertWarp.ts
+++ components/tools/L1Launcher/DeployContracts/convertWarp.ts
@@ -1,7 +1,7 @@
 // FIXME: this is a quick hack solution untill AvalancheJS supports this
 // Please don't copy this code to other projects!
 
-import { cb58ToHex } from "../utils/cb58";
+import { cb58ToBytes } from "../utils/cb58";
 
 interface MarshalSubnetToL1ConversionDataArgs {
     subnetId: string;
@@ -18,43 +18,78 @@ interface NodePOP {
     };
 }
 
-const BootstrapValidatorWeight = 100
+const BootstrapValidatorWeight = 100n;
+const codecVersion = 0;
 
-const codecVersion = 0
-export function MarshalSubnetToL1ConversionData(args: MarshalSubnetToL1ConversionDataArgs): string {
-    let result = ""
+function encodeUint16(num: number): Uint8Array {
+    const arr = new Uint8Array(2);
+    arr[0] = (num >> 8) & 0xff;
+    arr[1] = num & 0xff;
+    return arr;
+}
 
-    result += encodeUint16Hex(codecVersion);
-    result += cb58ToHex(args.subnetId, false);
-    result += cb58ToHex(args.managerChainID, false);
-    result += encodeVarBytesHex(args.managerAddress);
-    result += encodeInt32Hex(args.nonePopJsons.length);
-    for (const popJson of args.nonePopJsons) {
-        const { nodeID, nodePOP } = JSON.parse(popJson) as NodePOP;
-        result += encodeVarBytesHex(cb58ToHex(nodeID.split("-")[1], false));
-        result += unZerox(nodePOP.publicKey);
-        result += encodeInt64Hex(BootstrapValidatorWeight);
+function encodeInt32(num: number): Uint8Array {
+    const arr = new Uint8Array(4);
+    arr[0] = (num >> 24) & 0xff;
+    arr[1] = (num >> 16) & 0xff;
+    arr[2] = (num >> 8) & 0xff;
+    arr[3] = num & 0xff;
+    return arr;
+}
+
+function encodeInt64(num: bigint): Uint8Array {
+    const arr = new Uint8Array(8);
+    for (let i = 7; i >= 0; i--) {
+        arr[i] = Number(num & 0xffn);
+        num = num >> 8n;
     }
+    return arr;
+}
 
+function hexToBytes(hex: string): Uint8Array {
+    hex = hex.replace(/^0x/, '');
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+        bytes[i / 2] = parseInt(hex.slice(i, i + 2), 16);
+    }
+    return bytes;
+}
+
+function encodeVarBytes(bytes: Uint8Array): Uint8Array {
+    const lengthBytes = encodeInt32(bytes.length);
+    const result = new Uint8Array(lengthBytes.length + bytes.length);
+    result.set(lengthBytes);
+    result.set(bytes, lengthBytes.length);
     return result;
 }
 
-//4 bytes length + bytes
-const encodeVarBytesHex = (bytes: string) => {
-    bytes = unZerox(bytes);
-    return encodeInt32Hex(bytes.length / 2) + bytes;
+function concatenateUint8Arrays(...arrays: Uint8Array[]): Uint8Array {
+    const totalLength = arrays.reduce((acc, arr) => acc + arr.length, 0);
+    const result = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const arr of arrays) {
+        result.set(arr, offset);
+        offset += arr.length;
+    }
+    return result;
 }
 
-const encodeInt64Hex = (int: number) => {
-    return int.toString(16).padStart(16, '0');
-}
+export function MarshalSubnetToL1ConversionData(args: MarshalSubnetToL1ConversionDataArgs): string {
+    const parts: Uint8Array[] = [];
 
-const encodeInt32Hex = (int: number) => {
-    return int.toString(16).padStart(8, '0');
-}
+    parts.push(encodeUint16(codecVersion));
+    parts.push(cb58ToBytes(args.subnetId));
+    parts.push(cb58ToBytes(args.managerChainID));
+    parts.push(encodeVarBytes(hexToBytes(args.managerAddress)));
+    parts.push(encodeInt32(args.nonePopJsons.length));
 
-const encodeUint16Hex = (int: number) => {
-    return int.toString(16).padStart(4, '0');
-}
+    for (const popJson of args.nonePopJsons) {
+        const { nodeID, nodePOP } = JSON.parse(popJson) as NodePOP;
+        parts.push(encodeVarBytes(cb58ToBytes(nodeID.split("-")[1])));
+        parts.push(hexToBytes(nodePOP.publicKey));
+        parts.push(encodeInt64(BootstrapValidatorWeight));
+    }
 
-const unZerox = (hex: string) => hex.replace(/^0x/, '');
+    const result = concatenateUint8Arrays(...parts);
+    return Buffer.from(result).toString('hex');
+}
