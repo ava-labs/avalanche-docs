5a7d5789e3733d29fe6900f7c5956c9a9c7056d4
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2025-02-06 16:35:23 +0530

    add: rpc providers direct link

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/api-reference/rpc-providers',
+        destination: 'https://build.avax.network/integrations#RPC%20Providers',
+        permanent: true,
+      },  
+    ]
+  },
 };
 
 export default config;
