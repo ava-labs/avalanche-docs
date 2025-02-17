153cc257a5596f0ad0217463f0891f8f4ca0d331
sebaxor <sebasor@gmail.com>
2025-02-16 19:47:30 -0500

    feat(hackathons): integration apis

--- app/api/hackathons/[id]/route.ts
+++ app/api/hackathons/[id]/route.ts
@@ -1,2 +1,52 @@
-export { GET } from "./get";
-export { PUT } from "./put";
+import { db } from "@/drizzle/db/db";
+import { hackathons } from "@/drizzle/schema/schema";
+import { validateHackathon } from "@/services/hackathons";
+import { Hackathon } from "@/types/hackathons";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  try {
+    const { id } = await params;     
+    const hackathon = await db
+      .select()
+      .from(hackathons)
+      .where(eq(hackathons.id, id));
+
+    if (!hackathon.length) {
+      return NextResponse.json({ error: "Hackathon not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(hackathon[0]); 
+  } catch (error) {
+    console.error("Error in GET /api/hackathons/[id]:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+      const { id } = params; 
+      let editedHackathon  = (await req.json())as Partial<Hackathon>;      
+      const errors = validateHackathon(editedHackathon);
+      if (errors.length > 0) {
+        return NextResponse.json({ errors }, { status: 400 });
+      }
+      const existingHackathon = await db.select().from(hackathons).where(eq(hackathons.id, id));
+      if (!existingHackathon.length) {
+        return NextResponse.json({ error: "hackaton not found." }, { status: 404 });
+      }
+  
+      const updatedHackathon = await db
+        .update(hackathons)
+        .set(editedHackathon)
+        .where(eq(hackathons.id, id))
+        .returning();
+  
+      return NextResponse.json(updatedHackathon[0]); // Devolver el hackathon actualizado
+    } catch (error) {
+      console.error("Error in PUT /api/hackathons/[id]:", error);
+      return NextResponse.json({ error: `Internal Server Error : ${error}` }, { status: 500 });
+    }
+  }
--- app/api/hackathons/route.ts
+++ app/api/hackathons/route.ts
@@ -1,2 +1,118 @@
-export { GET } from "./get";
-export { POST } from "./post";
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/drizzle/db/db";
+import { hackathons } from "@/drizzle/schema/schema";
+import { eq, sql, and } from "drizzle-orm";
+
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = req.nextUrl;
+
+    const page = parseInt(searchParams.get("page") ?? "1", 10);
+    const pageSize = parseInt(searchParams.get("pageSize") ?? "10", 10);
+
+    if (isNaN(page) || page < 1 || isNaN(pageSize) || pageSize < 1) {
+      return NextResponse.json(
+        { error: "Pagination params invalid" },
+        { status: 400 }
+      );
+    }
+
+    const offset = (page - 1) * pageSize;
+
+    const location = searchParams.get("location");
+    const date = searchParams.get("date");
+    const status = searchParams.get("status");
+
+    const filters = [];
+    if (location) filters.push(eq(hackathons.location, location));
+    if (date) filters.push(eq(hackathons.date, date));
+    if (status) filters.push(eq(hackathons.status, status));
+
+    const whereCondition = filters.length ? and(...filters) : undefined;
+
+    const hackathonList = await db
+      .select()
+      .from(hackathons)
+      .where(whereCondition)
+      .limit(pageSize)
+      .offset(offset);
+    const totalResult = await db
+      .select({ count: sql<number>`COUNT(*)` })
+      .from(hackathons)
+      .where(whereCondition);
+
+    const totalHackathons = totalResult[0]?.count ?? 0;
+
+    return NextResponse.json({
+      hackathons: hackathonList,
+      total: totalHackathons,
+      page,
+      pageSize,
+    });
+  } catch (error) {
+    console.error("Error in GET /api/hackathons:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+
+
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const {
+      title,
+      description,
+      date,
+      location,
+      total_prizes,
+      tags,
+      status,
+      registration_deadline,
+      address,
+      agenda,
+      partners,
+      tracks,
+    } = body;
+
+    
+    if (!title || !description || !date || !location || !status || !registration_deadline) {
+      return NextResponse.json({ error: "Faltan campos obligatorios" }, { status: 400 });
+    }
+
+  
+    const newHackathon = await db
+      .insert(hackathons)
+      .values({
+        title,
+        description,
+        date,
+        location,
+        total_prizes,
+        tags,
+        status,
+        registration_deadline,
+        address,
+        agenda,
+        partners,
+        tracks,
+      })
+      .returning();
+
+    return NextResponse.json(
+      { message: "Hackathon creado", hackathon: newHackathon[0] },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error en POST /api/hackathons:", error);
+    return NextResponse.json(
+      { error: "Error interno del servidor" },
+      { status: 500 }
+    );
+  }
+}
