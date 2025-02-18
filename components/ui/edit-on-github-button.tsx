5210b105e3487970496a9754a807625b21d3e8f5
Martin Eckardt <m.eckardt@outlook.com>
2025-02-18 19:20:24 +0100

    Update components/ui/edit-on-github-button.tsx
    Signed-off-by: Martin Eckardt <m.eckardt@outlook.com>

--- components/ui/edit-on-github-button.tsx
+++ components/ui/edit-on-github-button.tsx
@@ -9,7 +9,7 @@ interface EditOnGithubButtonProps {
 export default function EditOnGithubButton({ path }: EditOnGithubButtonProps) {
   return (
     <a
-      href={`https://github.com/ava-labs/avalanche-docs/blob/master/${path}`}
+      href={`https://github.com/ava-labs/avalanche-docs/edit/master/${path}`}
       target="_blank"
       rel="noreferrer noopener"
       className={cn(buttonVariants({ variant: 'secondary' }), "gap-2 no-underline")}
