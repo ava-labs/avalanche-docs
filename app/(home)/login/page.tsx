6e6ce355c1c5568e2ca490fdefe145830d2b4e53
jhoan <jhoan_sebastian20@hotmail.com>
2025-02-24 21:09:26 -0500

    change: changes functions of svg into svg file,

--- app/(home)/login/page.tsx
+++ app/(home)/login/page.tsx
@@ -1,10 +1,6 @@
 import Formlogin from '@/components/login/FormLogin';
-import { signIn } from 'next-auth/react';
 
-export default function loginPage(): React.ReactElement {
-  async function handleSignIn() {
-    await signIn('google', { callbackUrl: `/` });
-  }
+export default function LoginPage(): React.ReactElement {
   return (
     <main className='container relative  px-2 py-4 lg:p-14 '>
       <div className='border border-zinc-800 shadow-sm bg-zinc-950 rounded-md'>
