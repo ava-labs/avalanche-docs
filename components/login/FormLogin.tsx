fee6c55ff0794be80c497f31e5c1bf25456fc937
anotherdev <joseluismanco37@gmail.com>
2025-03-02 14:52:32 -0500

    fix: minimal UI/Architecture improvements

--- components/login/FormLogin.tsx
+++ components/login/FormLogin.tsx
@@ -1,54 +1,117 @@
 'use client';
 
-
 import Link from 'next/link';
 import Image from 'next/image';
 
-import SocialMediaLoginButtons from './sections/social-media-login-buttons';
-import { FooterLogin } from './sections/footer-login';
-import { SignInInputs } from './sections/sing-in-inputs';
+import SocialLogin from './social-login/SocialLogin';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { useForm } from 'react-hook-form';
 
+const formSchema = z.object({
+  email: z.string().email('Please enter a valid email address'),
+});
 
 function Formlogin() {
+  const formMethods = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
 
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
 
   return (
     <main>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4  items-center">
-        <div className="hidden lg:flex pt-14 pb-14 justify-center items-center bg-zinc-900">
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4  items-center'>
+        <div className='hidden lg:block p-10 w-full h-full bg-zinc-900'>
           <Image
-            src={"/hackaton-platform-images/avalancheLoginLogo.svg"}
-            alt={"logo_avalanche "}
-            width="560"
-            height="685"
-          ></Image>
+            src='/hackaton-platform-images/avalancheLoginLogo.svg'
+            alt='logo_avalanche '
+            width='560'
+            height='685'
+          />
         </div>
 
-        <div className='justify-between p-10'>
-          <div className='py-2 px-4 gap-3'>
-            <Link
-              href="#"
-         
-              className="absolute top-5 lg:top-[9rem] right-0 left-0 lg:right-24  w-full lg:w-auto text-center lg:text-right lg:bg-transparent text-white lg:text-zinc-400 underline hover:text-gray-300 py-2 lg:py-0 px-4 lg:px-0 font-medium z-50"
-            >
-              Sign Up
-            </Link>
-          </div>
-          <div className="flex flex-col justify-center items-center   gap-2">
-            <div className="text-center ">
-              <h3 className="font-medium text-2xl">Sign in to your account</h3>
-              <p className="text-zinc-400 text-sm pt-2">
-                Sign up with email or choose another method.
+        <div className='justify-between p-10 bg-zinc-950'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <div className='text-center '>
+              <h3 className='font-medium text-2xl'>Sign in to your account</h3>
+              <p className='text-zinc-400 text-sm pt-2'>
+                Enter your email to receive a sign-in code
               </p>
             </div>
 
-            <div className="flex flex-col justify-center items-center space-y-6 w-full mt-6 max-w-[350px]">
-    
-              <SignInInputs/>
-              <SocialMediaLoginButtons/>
-              <FooterLogin/>
+            <div className='flex flex-col justify-center items-center space-y-6 w-full mt-6 max-w-[350px]'>
+              <div className='w-full'>
+                <Form {...formMethods}>
+                  <form onSubmit={formMethods.handleSubmit(onSubmit)} className='space-y-6 w-full'>
+                    <div className='space-y-2'>
+                      <FormField
+                        control={formMethods.control}
+                        name='email'
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <Input
+                                className='bg-transparent w-full'
+                                placeholder='name@example.com'
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <Button
+                      type='submit'
+                      variant='destructive'
+                      className='w-full bg-red-500 p-2 rounded'
+                    >
+                      SEND VERIFICATION CODE
+                    </Button>
+                  </form>
+                </Form>
+              </div>
+              <SocialLogin />
+              <div>
+                <footer className='pt-10'>
+                  <p className='text-zinc-400 items-center justify-center w-full max-w-[400px] text-center text-sm font-medium'>
+                    By clicking continue, you agree to our{' '}
+                    <Link
+                      href='#'
+                      target='_blank'
+                      className='underline text-white hover:text-gray-300'
+                    >
+                      Terms of Service
+                    </Link>{' '}
+                    and{' '}
+                    <Link
+                      href='#'
+                      target='_blank'
+                      className='underline text-white hover:text-gray-300'
+                    >
+                      Privacy Policy
+                    </Link>
+                    .
+                  </p>
+                </footer>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
