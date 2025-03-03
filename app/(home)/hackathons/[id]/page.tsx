c0d1e7cb64f657beb82f3bfe2437920db4e646e8
anotherdev <joseluismanco37@gmail.com>
2025-03-03 17:12:31 -0500

    fix: ux and layout

--- app/(home)/hackathons/[id]/page.tsx
+++ app/(home)/hackathons/[id]/page.tsx
@@ -135,163 +135,118 @@ export default async function HackathonPage({
   if (!hackathon) redirect('/hackathons');
 
   return (
-    <main className='container max-w-[1100px] px-2 py-4 lg:py-16'>
+    <main className='container px-2 py-4 lg:py-16'>
       {/* Outer Wrapper with Rounded Top Corners */}
-      <div className='flex flex-col border border-gray-700 rounded-t-lg overflow-hidden shadow-lg'>
+
+      <div className='flex flex-col '>
         {/* Menu Section */}
         <NavigationMenu items={menuItems} />
 
-        {/* Jumbotron Section */}
-        <div
-          className='relative bg-zinc-800 min-h-[500px] w-full'
-          id='overview'
-        >
-          <div className='absolute inset-0 w-full h-full'>
-            <Image
-              src='/builders-hub/hackathons/main_banner_img.png'
-              alt='Hackathon background'
-              fill
-              className='object-cover'
-              priority
-            />
-          </div>
-        </div>
+        <div className='px-8 pt-6'>
+          {/* Jumbotron Section */}
+          <Image
+            src='/builders-hub/hackathons/main_banner_img.png'
+            alt='Hackathon background'
+            width={1270}
+            height={760}
+            priority
+          />
 
-        <div className='p-6 space-y-8'>
-          {/* Schedule Section */}
-          <section>
-            <h2 className='text-4xl font-bold mb-8' id='schedule'>
-              Schedule
-            </h2>
-            <Divider />
-            <p>{getDateRange(hackathon.content.schedule)}</p>
-            <div className='flex justify-between gap-10 mt-4 min-w-full'>
-              <div className='flex items-center justify-center gap-10'>
-                <SearchEventInput />
-                <TimeZoneSelect />
+          <div className='p-8 flex flex-col gap-24'>
+            {/* Schedule Section */}
+            <section>
+              <h2 className='text-5xl font-bold mb-8' id='schedule'>
+                Schedule
+              </h2>
+              <Divider />
+              <p>{getDateRange(hackathon.content.schedule)}</p>
+              <div className='flex justify-between gap-10 mt-4 min-w-full'>
+                <div className='flex items-center justify-center gap-10'>
+                  <SearchEventInput />
+                  <TimeZoneSelect />
+                </div>
+                <DeadlineTimer events={hackathon.content.schedule} />
               </div>
-              <DeadlineTimer events={hackathon.content.schedule} />
-            </div>
-            <Divider />
-            {/* Schedule content will go here */}
+              <Divider />
+              {/* Schedule content will go here */}
 
-            {/* Group activities by day */}
-            <div className='relative overflow-x-auto'>
-              <div className='grid grid-flow-col auto-cols-[100%] md:auto-cols-[50%] gap-5'>
-                {Object.entries(groupActivitiesByDay(mockActivities)).map(
-                  ([date, activities], index) => (
-                    <div key={index} className='min-w-[300px]'>
-                      <h3 className='text-lg font-bold mb-4'>
-                        {new Date(date).toDateString()}
-                      </h3>
-                      <div className='max-h-[600px] overflow-y-auto pr-2'>
-                        <div className='space-y-4'>
-                          {activities.map((activity, index) => (
-                            <div
-                              key={index}
-                              className='bg-zinc-900 rounded-lg p-6 flex flex-col'
-                            >
-                              {/* <div className="text-sm text-zinc-400">
-                                {new Date(activity.date).toLocaleTimeString(
-                                  "en-US",
-                                  {
-                                    hour: "2-digit",
-                                    minute: "2-digit",
-                                  }
-                                )}
-                              </div> */}
-                              <div className='flex justify-between items-center mb-5'>
-                                <div>
-                                  <h4 className='text-2xl font-bold text-red-500'>
-                                    {activity.name}
-                                  </h4>
-                                  <p className='text-md text-zinc-400'>
-                                    {activity.host}
+              {/* Group activities by day */}
+              <div className='relative overflow-x-auto'>
+                <div className='grid grid-flow-col auto-cols-[100%] md:auto-cols-[50%] gap-5'>
+                  {Object.entries(groupActivitiesByDay(mockActivities)).map(
+                    ([date, activities], index) => (
+                      <div key={index} className='min-w-[300px]'>
+                        <h3 className='text-lg font-bold mb-4'>
+                          {new Date(date).toDateString()}
+                        </h3>
+                        <div className='max-h-[600px] overflow-y-auto pr-2'>
+                          <div className='space-y-4'>
+                            {activities.map((activity, index) => (
+                              <div
+                                key={index}
+                                className='bg-zinc-900 rounded-lg p-6 flex flex-col'
+                              >
+                                <div className='flex justify-between items-center mb-5'>
+                                  <div>
+                                    <h4 className='text-2xl font-bold text-red-500'>
+                                      {activity.name}
+                                    </h4>
+                                    <p className='text-md text-zinc-400'>
+                                      {activity.host}
+                                    </p>
+                                  </div>
+                                  <p className='text-sm bg-zinc-50 rounded-full text-black px-2.5 py-0.5'>
+                                    {activity.level}
                                   </p>
                                 </div>
-                                <p className='text-sm bg-zinc-50 rounded-full text-black px-2.5 py-0.5'>
-                                  {activity.level}
-                                </p>
-                              </div>
-                              <div className='flex justify-between items-center text-white mt-2'>
-                                {/* <span className="text-sm">
+                                <div className='flex justify-between items-center text-white mt-2'>
+                                  {/* <span className="text-sm">
                                   {activity.stage}
                                 </span>
                                 <span className="text-sm">
                                   {activity.duration}
                                 </span> */}
+                                </div>
                               </div>
-                            </div>
-                          ))}
+                            ))}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  )
-                )}
+                    )
+                  )}
+                </div>
               </div>
-            </div>
-          </section>
-          {/* Info Section */}
-          <section>
-            <h2 className='text-4xl font-bold mb-8' id='info'>
-              Info
-            </h2>
-            <Divider />
-            {/* Info content will go here */}
-          </section>
-          {/* Partners Section */}
-          <section>
-            <h2 className='text-xl font-bold mb-4' id='partners'>
-              Partners
-            </h2>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-6'>
-              {hackathon.content.partners.map((partner, index) => (
-                <div
-                  key={index}
-                  className='flex flex-col items-center justify-center border border-gray-400 rounded-lg p-4'
-                >
-                  <img
-                    src={partner.logo}
-                    alt={partner.name}
-                    className='w-50 min-h-[120px] object-contain'
-                  />
-                </div>
-              ))}
-            </div>
-          </section>
-          {/* Tracks Section */}
-          <section>
-            <h2 className='text-4xl font-bold mb-8' id='tracks'>
-              Pizes & Tracks
-            </h2>
-            <Divider />
-            <div className='h-px grid grid-cols-1 xl:grid-cols-2 gap-4'>
-              {hackathon.content.tracks.map((track, index) => (
-                <div
-                  key={index}
-                  className='border border-white rounded-lg p-4 flex flex-col gap-4'
-                >
-                  <h3 className='text-lg font-bold'>{track.name}</h3>
-
-                  <p className='text-sm text-gray-300'>{track.description}</p>
-
-                  <div className='flex justify-between items-center text-white mt-2'>
-                    <span className='text-sm'>prizes</span>
-                    <div className='flex items-center gap-1'>
-                      <Trophy className='w-4 h-4' />
-                      <span className='text-sm'>${track.total_reward}</span>
-                    </div>
+            </section>
+            {/* Info Section */}
+            <section>
+              <h2 className='text-4xl font-bold mb-8' id='info'>
+                Info
+              </h2>
+              <Divider />
+              {/* Info content will go here */}
+            </section>
+            {/* Partners Section */}
+            <section>
+              <h2 className='text-xl font-bold mb-4' id='partners'>
+                Partners
+              </h2>
+              <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-6'>
+                {hackathon.content.partners.map((partner, index) => (
+                  <div
+                    key={index}
+                    className='flex flex-col items-center justify-center border border-gray-400 rounded-lg p-4'
+                  >
+                    <img
+                      src={partner.logo}
+                      alt={partner.name}
+                      className='w-50 min-h-[120px] object-contain'
+                    />
                   </div>
-
-                  <div className='mt-4'>
-                    <span className='bg-gray-800 text-white text-xs font-semibold px-3 py-1 rounded-full'>
-                      {track.partner}
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
+                ))}
+              </div>
+            </section>
+            {/* Tracks Section */}
+          </div>
         </div>
       </div>
       <div className='flex justify-end mt-4'>
@@ -339,7 +294,9 @@ function getDateRange(activities: ScheduleActivity[]): string {
   return `${formatter.format(earliestDate)} - ${formatter.format(latestDate)}`;
 }
 
-function groupActivitiesByDay(activities: ScheduleActivity[]): GroupedActivities {
+function groupActivitiesByDay(
+  activities: ScheduleActivity[]
+): GroupedActivities {
   return activities.reduce((groups: GroupedActivities, activity) => {
     // Format the date to YYYY-MM-DD to use as key
     const date = new Date(activity.date);
--- components/hackathons/NavigationMenu.tsx
+++ components/hackathons/NavigationMenu.tsx
@@ -11,7 +11,7 @@ interface NavigationMenuProps {
 
 export function NavigationMenu({ items }: NavigationMenuProps) {
   return (
-    <div className="p-4 border-b border-gray-700">
+    <div className="py-4 px-8 border-b border-gray-700">
       <nav className="text-sm">
         <ul className="flex space-x-4 px-4 py-2 text-sm">
           {items.map((item, index) => (
