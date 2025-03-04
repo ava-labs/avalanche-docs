1126958281527b9313de851cfc9e9ccb7447668c
simon0820s <simon0820s@gmail.com>
2025-03-04 10:37:34 -0500

    feat(pricesTracks): complete view

--- components/hackathons/hackathon/sections/Tracks.tsx
+++ components/hackathons/hackathon/sections/Tracks.tsx
@@ -1,3 +1,4 @@
+import { DynamicIcon } from "lucide-react/dynamic";
 import { Divider } from "@/components/ui/divider";
 import { HackathonHeader } from "@/types/hackathons";
 import { Crown } from "lucide-react";
@@ -16,27 +17,29 @@ function Tracks({ hackathon }: { hackathon: HackathonHeader }) {
           2025!
         </p>
       </div>
-      <div className="absolute left-0 mt-24">
-        <div className="relative left-0 w-full py-32 px-20 grid grid-cols-4 items-center content-center gap-8 bg-zinc-200 opacity-15">
-          <div className="w-1/5 absolute top-[-5.5rem] left-1/2 transform -translate-x-1/2 h-44 p-8 flex flex-col content-center items-center bg-red-300 rounded-xl">
-            <div className="p-2 rounded-full bg-white">
-              <Crown color="#FF394A" />
-            </div>
-            <h2 className="text-xl font-bold text-zinc-900">
-              ${hackathon.total_prizes.toLocaleString("en-US")}
-            </h2>
-            <p className="text-sm text-zinc-900">Total price pool</p>
+      <div className="relative py-32 grid grid-cols-4 content-center gap-8 mt-24">
+        <div className="absolute -z-10 w-screen h-full left-1/2 transform -translate-x-1/2 bg-zinc-200"></div>
+        <div className="w-1/4 absolute top-[-5.5rem] left-1/2 transform -translate-x-1/2 p-8 flex flex-col content-center items-center bg-red-300 rounded-xl">
+          <div className="p-2 rounded-full bg-white mb-4">
+            <Crown color="#FF394A" />
           </div>
-          {hackathon.content.tracks.map((track, index) => (
-            <div
-              key={index}
-              className="relative w-full h-44 p-8 bg-zinc-900 rounded-xl border-zinc-800 border-2"
-            >
-              <h2 className="text-xl font-bold">{track.name}</h2>
-              <p className="text-sm text-zinc-400">{track.description}</p>
-            </div>
-          ))}
+          <h2 className="text-xl font-bold text-zinc-900">
+            ${hackathon.total_prizes.toLocaleString("en-US")}
+          </h2>
+          <p className="text-sm text-zinc-900">Total price pool</p>
         </div>
+        {hackathon.content.tracks.map((track, index) => (
+          <div
+            key={index}
+            className="w-full h-44 p-8 bg-zinc-900 rounded-xl border-zinc-800 border-2"
+          >
+            <div className="flex justify-between items-center gap-2">
+              <h2 className="text-xl font-bold">{track.name}</h2>
+              <DynamicIcon name={track.icon as any} color="#A7A7B0" size={16} />
+            </div>
+            <p className="text-sm text-zinc-400">{track.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
