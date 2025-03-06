49ebfff1264c35398f987f5138cc80ce7fb9203b
sebaxor <sebasor@gmail.com>
2025-03-06 04:00:25 -0500

    feat(hackathons): added pages layer

---
+++ app/(home)/hackathons/[id]/page.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import {
+  getFilteredHackathons,
+  getHackathon,
+} from "@/server/services/hackathons";
+import { Divider } from "@/components/ui/divider";
+import Image from "next/image";
+import { NavigationMenu } from "@/components/hackathons/NavigationMenu";
+import Schedule from "@/components/hackathons/hackathon/sections/Schedule";
+import Tracks from "@/components/hackathons/hackathon/sections/Tracks";
+import Sponsors from "@/components/hackathons/hackathon/sections/Sponsors";
+import Submission from "@/components/hackathons/hackathon/sections/Submission";
+import Resources from "@/components/hackathons/hackathon/sections/Resources";
+import Community from "@/components/hackathons/hackathon/sections/Community";
+import Speakers from "@/components/hackathons/hackathon/sections/Speakers";
+export const revalidate = 60;
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const { hackathons } = await getFilteredHackathons({});
+  return hackathons.map((hackathon) => ({
+    id: hackathon.id,
+  }));
+}
+
+export default async function HackathonPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const hackathon = await getHackathon(id);
+  console.log({ hackathon });
+
+  const menuItems = [
+    { name: "Overview", ref: "overview" },
+    { name: "Schedule", ref: "schedule" },
+    { name: "Prizes & Tracks", ref: "tracks" },
+    { name: "Sponsors", ref: "sponsors" },
+    { name: "Submission", ref: "submission" },
+    { name: "Resources", ref: "resources" },
+    { name: "Speakers", ref: "speakers" },
+  ];
+
+  if (!hackathon) redirect("/hackathons");
+
+  return (
+    <main className="container px-2 py-4 lg:py-16">
+      <div className="flex gap-4 items-center">
+        <Image
+          src="/temp/hackathon-icon.png"
+          alt="Hackathon background"
+          width={40}
+          height={40}
+        />
+        <span className="text-xl font-bold">{hackathon.title}</span>{" "}
+        <Button asChild>
+          <Link href={`/hackathon/registration-form`}>Join now</Link>
+        </Button>
+      </div>
+
+      <div className="flex flex-col mt-8 ">
+        <NavigationMenu items={menuItems} />
+        <div className="px-8 pt-6 ">
+          <Link href={`/hackathons/${id}`}>
+            <Image
+              src="/builders-hub/hackathons/main_banner_img.png"
+              alt="Hackathon background"
+              width={1270}
+              height={760}
+              priority
+            />
+          </Link>
+          <div className="p-8 flex flex-col gap-24">
+            <Schedule hackathon={hackathon} />
+            <Divider />
+            <Tracks hackathon={hackathon}></Tracks>
+            <Divider />
+            <Sponsors hackathon={hackathon}></Sponsors>
+            <Divider />
+            <Submission hackathon={hackathon}></Submission>
+            <Divider />
+            <Resources hackathon={hackathon}></Resources>
+            <Divider />
+            <Community hackathon={hackathon}></Community>
+            <Divider />
+            <Speakers hackathon={hackathon}></Speakers> 
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-end mt-4">
+        <Link href={`/hackathons/${id}/admin-panel`}>
+          <Button>Edit Hackathon</Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
