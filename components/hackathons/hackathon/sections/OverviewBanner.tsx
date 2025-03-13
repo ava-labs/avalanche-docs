c14a13e3aa140ecc1bc3ae563486d987a6238aae
simon0820s <simon0820s@gmail.com>
2025-03-13 15:34:04 -0500

    feat(bannerOverview): fix styles issues

--- components/hackathons/hackathon/sections/OverviewBanner.tsx
+++ components/hackathons/hackathon/sections/OverviewBanner.tsx
@@ -24,12 +24,12 @@ export default function OverviewBanner({ hackathon, id }: Props) {
       ? `${format(startDate, "MMMM d")} - ${format(endDate, "d, yyyy")}`
       : `${format(startDate, "MMMM d")} - ${format(endDate, "MMMM d, yyyy")}`;
   return (
-    <div className="z-10 h-full absolute flex flex-col justify-end bottom-20 left-[10%]  mb-[-30px]">
+    <div className="z-10 h-full w-[46%] absolute flex flex-col justify-end bottom-20 left-[10%]  mb-[-30px]">
       <h1 className="text-6xl text-zinc-50 font-bold mb-2">
         {hackathon.title}
       </h1>
-      <div className="w-[70%] max-w-80">
-        <p className="text-base text-zinc-50">{hackathon.description}</p>
+      <p className="text-base text-zinc-50">{hackathon.description}</p>
+      <div className="max-w-80">
         <h2
           className="mt-6 mb-12 text-6xl font-bold text-red-500"
           style={{ textShadow: "0px 4px 6px #9F2428" }}
@@ -38,12 +38,12 @@ export default function OverviewBanner({ hackathon, id }: Props) {
         </h2>
         <Button
           variant={"secondary"}
-          className="w-full mb-20 bg-red-500 rounded-md text-zinc-100"
+          className="w-full mb-20 bg-red-500 border-none text-zinc-100 rounded-md"
         >
           <Link href={`/hackathons/registration-form`}>JOIN NOW</Link>
         </Button>
-        <div className="flex flex-col w-[90%]">
-          <div className="flex flex-col gap-2 w-[75%]">
+        <div className="flex flex-col">
+          <div className="flex flex-col gap-2 max-w-[60%]">
             <div className="flex justify-between gap-8 text-gray-400">
               <Calendar size={20} color="#F5F5F9" />
               <span className="text-sm text-zinc-50">{formattedDate}</span>
@@ -55,7 +55,7 @@ export default function OverviewBanner({ hackathon, id }: Props) {
               </span>
             </div>
           </div>
-          <div className="flex justify-center gap-4 mt-4">
+          <div className="max-w-[90%] flex justify-center flex-wrap gap-x-4 gap-y-2 mt-4">
             {hackathon.tags.map((tag, index) => (
               <Badge
                 key={index}
@@ -65,11 +65,11 @@ export default function OverviewBanner({ hackathon, id }: Props) {
               </Badge>
             ))}
           </div>
-          <div className="flex justify-between gap-4 mt-8">
+          <div className="flex justify-between gap-4 mt-4 max-w-[90%]">
             <div className="flex gap-2 text-gray-400">
               <Users size={20} color="#F5F5F9" />
               <span className="text-sm text-zinc-50">
-                {hackathon.content.participants}
+                {hackathon.participants}
               </span>
             </div>
             <HackathonStatus status={hackathon.status} />
