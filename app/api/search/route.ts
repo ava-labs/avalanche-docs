b6ee455d1949a6d2c821002385a8a32e97003e4e
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2025-03-14 19:00:14 +0530

    redo search config

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -11,6 +11,31 @@ export const { GET } = createSearchAPI('advanced', {
         id: page.url,
         structuredData: loadedData.structuredData
       }
+    }),
+    ...academy.getPages().map((page) => {
+      return {
+        title: page.data.title,
+        url: page.url,
+        id: page.url,
+        structuredData: page.data.structuredData
+      }
+    }),
+    ...integration.getPages().map(async (page) => {
+      const loadedData = await page.data.load()
+      return {
+        title: page.data.title,
+        url: page.url,
+        id: page.url,
+        structuredData: loadedData.structuredData
+      }
+    }),
+    ...guide.getPages().map((page) => {
+      return {
+        title: page.data.title,
+        url: page.url,
+        id: page.url,
+        structuredData: page.data.structuredData
+      }
     })
   ])
 })
