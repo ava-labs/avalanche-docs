c75bdd864cf2734afe61a5db3b7ed6c403101192
Ashutosh Tripathi <39340292+ashucoder9@users.noreply.github.com>
2025-03-14 18:50:02 +0530

    check with only docs

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -11,31 +11,6 @@ export const { GET } = createSearchAPI('advanced', {
         id: page.url,
         structuredData: loadedData.structuredData
       }
-    }),
-    ...academy.getPages().map((page) => {
-      return {
-        title: page.data.title,
-        url: page.url,
-        id: page.url,
-        structuredData: page.data.structuredData
-      }
-    }),
-    ...integration.getPages().map(async (page) => {
-      const loadedData = await page.data.load()
-      return {
-        title: page.data.title,
-        url: page.url,
-        id: page.url,
-        structuredData: loadedData.structuredData
-      }
-    }),
-    ...guide.getPages().map((page) => {
-      return {
-        title: page.data.title,
-        url: page.url,
-        id: page.url,
-        structuredData: page.data.structuredData
-      }
     })
   ])
 })
