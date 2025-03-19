48e78e2b4f893000eb865704e9aede0ee3311863
anotherdev <joseluismanco37@gmail.com>
2025-03-19 00:44:58 -0500

    feat: add red variant buttons fix: submission buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        red: "bg-red-500 hover:bg-red-600 text-white"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
